68cf148c976c77e5e13a5cf6eca8f25e293f2402
adrian <ongadrian98@gmail.com>
2019-10-20 00:15:45 +0800

    Added roll dice command

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,12 +1,28 @@
 import discord
 from discord.ext import commands
 
+import random
+
 
 def create_bot(bot_prefix, self_bot):
     client = commands.Bot(command_prefix=bot_prefix, self_bot=self_bot)
 
     @client.event
     async def on_ready():
-        print('We have logged in as {0.user}'.format(client))
+        await client.change_presence(activity=discord.Game(name='Doto'))
+        print('We have logged in as {}'.format(client.user))
+
+    @client.command(name='roll',
+                    description="This command accepts two parameters, start number and end number. Randoms both",
+                    brief="Rolls a random number (Default 1-100)",
+                    aliases=['roll_die', 'roll_dice'])
+    async def roll(ctx, start=1, end=100):
+        result = random.randint(int(start), int(end))
+        await ctx.send(str(result) + ctx.message.author.mention)
+
+    @roll.error
+    async def roll_error(ctx, error):
+        if isinstance(error, commands.BadArgument):
+            await ctx.send("Invalid arguments provided")
 
     return client
--- run.py
+++ run.py
@@ -4,5 +4,5 @@ import os
 bot_prefix = ("!", "?")
 client = create_bot(bot_prefix=bot_prefix, self_bot=False)
 
-
-client.run(os.getenv("bot_token", None))
+if __name__ == '__main__':
+    client.run(os.getenv("bot_token", None))
