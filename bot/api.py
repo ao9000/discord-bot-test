1281ee64063b044f5cd0f706fe988fd928bf7a8c
adrian <ongadrian98@gmail.com>
2019-10-27 15:45:40 +0800

    Added error handling decorators for API methods

--- bot/api.py
+++ bot/api.py
@@ -1,8 +1,8 @@
 from bot.helper_functions import url_join
+from bot.exceptions import APIHandler
 
 import requests
 import os
-import json
 
 # Base url
 discord_base_url = "https://discordapp.com/api"
@@ -10,17 +10,18 @@ opendota_base_url = "https://api.opendota.com/api"
 
 
 # Discord API
+@APIHandler
 def discord_api_get_user_profile(version, discord_id):
     url = url_join(discord_base_url, "v{}".format(version), "users/{}/profile".format(discord_id))
 
     header = {'Authorization': os.getenv('discord_token_user', None)}
     request = requests.get(url=url, headers=header)
 
-    response = json.loads(request.text)
-    return response
+    return request
 
 
 # OpenDota API
+@APIHandler
 def opendota_api_get_player_profile(steam_id_32):
     url = url_join(opendota_base_url, "players/{}".format(steam_id_32))
 
@@ -30,5 +31,4 @@ def opendota_api_get_player_profile(steam_id_32):
 
     request = requests.get(url=req.url)
 
-    response = json.loads(request.text)
-    return response
+    return request
