bc5b302af860d707b7c1e3c1574c59dd8b6fd4e1
adrian <ongadrian98@gmail.com>
2019-11-15 11:05:47 +0800

    Added pagination handler

--- bot/handler.py
+++ bot/handler.py
@@ -1,10 +1,16 @@
 import json
 import time
+import discord
+import nltk
 
 from bot.exceptions import APIError, APIMonthlyLimitReachedError
 
+# NLTK resources
+nltk.download('punkt')
+
 
 # Decorators
+# API handlers
 def GeneralAPIHandler(func):
     def func_wrapper(*args, **kwargs):
         request = func(*args, **kwargs)
@@ -37,3 +43,34 @@ def OpenDotaAPIHandler(func):
             raise APIError("Unknown API error")
 
     return func_wrapper
+
+
+# Pagination handlers
+def PaginationHandlerMeta(character_limit):
+    def PaginationHandler(func):
+        async def func_wrapper(*args, **kwargs):
+            # Await coroutine response
+            response = await func(*args, **kwargs)
+
+            for arg in args:
+                if isinstance(arg, discord.ext.commands.context.Context):
+                    ctx = arg
+                    break
+            # Text tokenization
+            if len(response) > character_limit:
+                response_tokenized = nltk.word_tokenize(response)
+
+            # Construct paginated response
+                start_index = 0
+                for end_index, _ in enumerate(response_tokenized, start=1):
+                    if len(" ".join([word for word in response_tokenized[start_index:end_index]])) > character_limit:
+                        await ctx.send(" ".join([word for word in response_tokenized[start_index:end_index-1]]))
+                        start_index = end_index - 1
+                    elif end_index == len(response_tokenized):
+                        await ctx.send(" ".join([word for word in response_tokenized[start_index:end_index]]))
+                return
+            else:
+                return await ctx.send(response)
+
+        return func_wrapper
+    return PaginationHandler
