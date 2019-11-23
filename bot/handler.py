41500992905441e4976b60b0ccfee12e0b99f27a
adrian <ongadrian98@gmail.com>
2019-11-24 00:15:29 +0800

    Added pagination handler for embed tables

--- bot/handler.py
+++ bot/handler.py
@@ -87,58 +87,102 @@ def PaginationHandlerMeta(arg):
         return PaginationHandler
 
 
-def EmbedPaginationHandler(func):
-    async def func_wrapper(*args, **kwargs):
-        # Await coroutine response
-        response = await func(*args, **kwargs)
+def EmbedPaginationHandlerMeta(arg):
+    def EmbedPaginationHandler(func):
+        async def func_wrapper(*args, **kwargs):
+            # Await coroutine response
+            response = await func(*args, **kwargs)
 
-        for arg in args:
-            if isinstance(arg, discord.ext.commands.context.Context):
-                ctx = arg
-                break
+            for arg in args:
+                if isinstance(arg, discord.ext.commands.context.Context):
+                    ctx = arg
+                    break
 
-        # Embed character limit checks
-        # For reference - https://discordapp.com/developers/docs/resources/channel#embed-limits
-        if len(response['title']) > 256:
-            raise CharacterLimitExceededError("Embed title character limit exceeded")
-        elif len(response['description']) > 2048:
-            raise CharacterLimitExceededError("Embed description character limit exceeded")
-        elif len(response['column_value']) > 25:
-            raise CharacterLimitExceededError("Embed field object limit exceeded")
-        elif any(len(header) > 256 for header in response['column_header']):
-            raise CharacterLimitExceededError("Embed column header character limit exceeded")
-        elif len(response['footer']) > 2048:
-            raise CharacterLimitExceededError("Embed footer character limit exceeded")
-        elif len(response['author']) > 256:
-            raise CharacterLimitExceededError("Embed author name character exceeded")
-        elif any(len(value) > 1024 for value in response['column_value']):
-            for index, value in enumerate(copy.deepcopy(response['column_value']), start=0):
+            # Embed character limit checks
+            # For reference - https://discordapp.com/developers/docs/resources/channel#embed-limits
+            if len(response['title']) > 256:
+                raise CharacterLimitExceededError("Embed title character limit exceeded")
+            elif len(response['description']) > 2048:
+                raise CharacterLimitExceededError("Embed description character limit exceeded")
+            elif len(response['column_value']) > 25:
+                raise CharacterLimitExceededError("Embed field object limit exceeded")
+            elif any(len(header) > 256 for header in response['column_header']):
+                raise CharacterLimitExceededError("Embed column header character limit exceeded")
+            elif len(response['footer']) > 2048:
+                raise CharacterLimitExceededError("Embed footer character limit exceeded")
+            elif len(response['author']) > 256:
+                raise CharacterLimitExceededError("Embed author name character exceeded")
+            elif any(len(value) > 1024 for value in response['column_value']):
+                if table:
+                    # Text tokenization (New line separator)
+                    response['column_value'] = [value.split("\n") for value in response['column_value']]
 
-                # Text tokenization
-                value_tokenized = nltk.word_tokenize(value)
+                    # Construct paginated response
+                    temp = list()
+                    for row in zip(*response['column_value']):
 
-                # Construct paginated response
-                temp = list()
-                start_index = 0
-                for end_index, _ in enumerate(value_tokenized, start=1):
-                    if len(" ".join([word for word in value_tokenized[start_index:end_index]])) > 1024:
-                        temp.append(" ".join([word for word in value_tokenized[start_index:end_index - 1]]))
-                        start_index = end_index - 1
-                    elif end_index == len(value_tokenized):
-                        temp.append(" ".join([word for word in value_tokenized[start_index:end_index]]))
+                        if temp:
+                            temp = ["{}\n{}".format(a, b) for a, b in zip(temp, list(row))]
+                        else:
+                            temp = list(row)
 
-                response['column_value'][index] = temp
-        else:
-            response['column_value'] = [response['column_value']]
+                        if any(len(value) > 1024 for value in temp):
+                            # Removing last addition
+                            temp = ["\n".join(word) for word in [col.split("\n")[:-1] for col in temp]]
+                            embed = discord_create_embed_table(title=response['title'],
+                                                               column_header=response['column_header'],
+                                                               column_value=[value for value in temp],
+                                                               description=response['description'],
+                                                               footer=response['footer'],
+                                                               author=response['author']
+                                                               )
+                            await ctx.send(embed=embed)
+                            # Retain overlapping row
+                            temp = list(row)
 
-        for index in range(0, max(len(value) for value in response['column_value'])-1):
-            embed = discord_create_embed_table(title=response['title'],
-                                               column_header=response['column_header'],
-                                               column_value=[value[index] for value in response['column_value']],
-                                               description=response['description'],
-                                               footer=response['footer'],
-                                               author=response['author']
-                                               )
-            await ctx.send(embed=embed)
+                else:
+                    # Text tokenization
+                    response['column_value'] = [nltk.word_tokenize(value) for value in response['column_value']]
 
-    return func_wrapper
+                    # Construct paginated response
+                    for index, value in enumerate(copy.deepcopy(response['column_value']), start=0):
+                        temp = list()
+                        start_index = 0
+                        for end_index, _ in enumerate(value, start=1):
+                            if len(" ".join([word for word in value[start_index:end_index]])) > 1024:
+                                temp.append(" ".join([word for word in value[start_index:end_index - 1]]))
+                                start_index = end_index - 1
+                            elif end_index == len(value):
+                                temp.append(" ".join([word for word in value[start_index:end_index]]))
+                        response['column_value'][index] = temp
+
+                    for index in range(0, max(len(value) for value in response['column_value']) - 1):
+                        embed = discord_create_embed_table(title=response['title'],
+                                                           column_header=response['column_header'],
+                                                           column_value=[value[index] for value in response['column_value']],
+                                                           description=response['description'],
+                                                           footer=response['footer'],
+                                                           author=response['author']
+                                                           )
+                        await ctx.send(embed=embed)
+
+            else:
+                embed = discord_create_embed_table(title=response['title'],
+                                                   column_header=response['column_header'],
+                                                   column_value=response['column_value'],
+                                                   description=response['description'],
+                                                   footer=response['footer'],
+                                                   author=response['author']
+                                                   )
+                await ctx.send(embed=embed)
+
+        return func_wrapper
+
+    if callable(arg):
+        # No argument provided
+        table = False
+        return EmbedPaginationHandler(arg)
+    else:
+        # Argument provided
+        table = arg
+        return EmbedPaginationHandler
