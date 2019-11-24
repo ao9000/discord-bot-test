86588c4fd959c4bdbb71141e7750fa086a6ba8db
adrian <ongadrian98@gmail.com>
2019-11-24 13:35:49 +0800

    Used handlers in some functions

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,6 +1,8 @@
-from bot.api import discord_api_get_user_profile, opendota_api_get_player_profile, steam_api_get_current_player_count
+from bot.api import discord_api_get_user_profile, opendota_api_get_player_profile, steam_api_get_current_player_count, \
+    opendota_api_get_mmr_distribution
 from bot.helper_functions import steam_id_64_to_steam_id_32, opendota_rank_tier_to_medal_name, \
     discord_create_embed_table
+from bot.handler import PaginationHandlerMeta, EmbedPaginationHandlerMeta
 
 import discord
 from discord.ext import commands
@@ -59,11 +61,12 @@ def create_bot(bot_prefix, self_bot):
                     description="Using Discord-OpenDota integration to retrieve the rank of members in the server",
                     brief="Displays the server medal ranking chart",
                     aliases=['dota2_member_medal'])
+    @EmbedPaginationHandlerMeta(table=True)
     async def dota2_server_medal(ctx):
         response_dict = {}
 
-        # members = client.get_all_members()
-        members = ctx.guild.members
+        members = client.get_all_members()
+        # members = ctx.guild.members
         for member in members:
             if not member.bot:
                 user_object = discord_api_get_user_profile(os.getenv("discord_api_version", 6), member.id)
@@ -73,13 +76,23 @@ def create_bot(bot_prefix, self_bot):
                     for connection in connection_object:
                         if connection['type'] == 'steam':
                             steam_id_64 = connection['id']
-                            opendota_profile_object = opendota_api_get_player_profile(steam_id_64_to_steam_id_32(steam_id_64))
+                            opendota_profile_object = opendota_api_get_player_profile(
+                                steam_id_64_to_steam_id_32(steam_id_64))
                             dota2_rank_tier = opendota_profile_object['rank_tier']
                             leaderboard_rank = opendota_profile_object['leaderboard_rank']
-                            response_dict[member.name + "#" + member.discriminator] = [dota2_rank_tier if dota2_rank_tier else 0, leaderboard_rank if leaderboard_rank else math.inf]
+                            response_dict[member.name + "#" + member.discriminator] = [
+                                dota2_rank_tier if dota2_rank_tier else 0,
+                                leaderboard_rank if leaderboard_rank else math.inf]
 
         if not response_dict:
-            await ctx.send("No members found" + ctx.message.author.mention)
+            return {
+                "title": "Medal Ranking",
+                "column_header": ["Error"],
+                "column_value": ["No member data found"],
+                "description": "Dota 2 medal distribution in the server",
+                "footer": "Updated on {}".format(datetime.datetime.now())
+            }
+
         else:
             member_names = ""
             medals = ""
@@ -90,18 +103,55 @@ def create_bot(bot_prefix, self_bot):
             column_value = [member_names, medals]
             column_header = ["Discord name", "Medal"]
 
-            embed = discord_create_embed_table(title="Medal Ranking", column_header=column_header,
-                                               column_value=column_value,
-                                               description="Dota 2 medal distribution in the server",
-                                               footer="Updated on {}".format(datetime.datetime.now()))
+            return {
+                "title": "Medal Ranking",
+                "column_header": column_header,
+                "column_value": column_value,
+                "description": "Dota 2 medal distribution in the server",
+                "footer": "Updated on {}".format(datetime.datetime.now())
+            }
 
-            await ctx.send(ctx.message.author.mention, embed=embed)
-
-    @client.command(name='dota2_current_player_count',
+    @client.command(name='dota2_player_count',
                     description="Using the official Valve API, retrieve the current number of players on Dota 2",
                     brief="Displays the Dota 2 player count")
+    @PaginationHandlerMeta
     async def dota2_current_player_count(ctx):
-        player_count = steam_api_get_current_player_count(os.getenv("steam_api_version", 1), 570)['response']['player_count']
-        await ctx.send("There is currently {:,} players in Dota 2".format(player_count) + ctx.message.author.mention)
+        player_count = steam_api_get_current_player_count(os.getenv("steam_api_version", 1), 570)['response'][
+            'player_count']
+        return "There is currently {:,} players in Dota 2".format(player_count)
+
+    @client.command(name='dota2_mmr_dist_country',
+                    description="Using the OpenDota API, retrieve the mmr distribution for each country and sort them in descending order",
+                    brief="Displays the Dota 2 mmr distribution by country")
+    @EmbedPaginationHandlerMeta(table=True)
+    async def dota2_mmr_dist_country(ctx):
+        mmr_dist_list = opendota_api_get_mmr_distribution()['country_mmr']['rows']
+
+        if not mmr_dist_list:
+            return {
+                "title": "Country MMR distribution",
+                "column_header": ["Error"],
+                "column_value": ["No data found"],
+                "description": "Dota 2 MMR distribution by country",
+                "footer": "Updated on {}".format(datetime.datetime.now())
+            }
+
+        else:
+            country_names = ""
+            country_avg_mmr = ""
+            for country_mmr_dict in sorted(mmr_dist_list, key=lambda x: x['avg'], reverse=True):
+                country_names += country_mmr_dict['common'] + '\n'
+                country_avg_mmr += country_mmr_dict['avg'] + '\n'
+
+            column_value = [country_names, country_avg_mmr]
+            column_header = ["Country", "Avg MMR"]
+
+            return {
+                "title": "Country MMR distribution",
+                "column_header": column_header,
+                "column_value": column_value,
+                "description": "Dota 2 MMR distribution by country",
+                "footer": "Updated on {}".format(datetime.datetime.now())
+            }
 
     return client
